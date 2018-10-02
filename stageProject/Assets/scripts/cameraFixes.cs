f54740d7a5e3b05c8c7a50cc12dbbec6238820aa
Martin van Lijden <dieselfreakje@live.nl>
2018-10-02 14:02:26 +0200

    initial setup

---
+++ stageProject/Assets/scripts/cameraFixes.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class cameraFixes : MonoBehaviour {
+
+    public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+    public RotationAxes axes = RotationAxes.MouseXAndY;
+    public float sensitivityX = 15F;
+    public float sensitivityY = 15F;
+    public float minimumX = -360F;
+    public float maximumX = 360F;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    private Rigidbody rb;
+    float rotationY = 0F;
+    
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        // Make the rigid body not change rotation
+        if (rb)
+            rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+}
---
+++ stageProject/Assets/scripts/playerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class playerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    public int velocid = 70;
+    private Vector3 moveDirection;
+    private float distance;
+    private int lpatLayer;
+    public float speed = 6.0F;
+    public float jumpSpeed = 8.0F;
+    public float gravity = 20.0F;
+    public Animator move;
+
+    // Use this for initialization  
+    void Start()
+    {
+        move = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // loop naar voren
+        if (Input.GetKey(KeyCode.W))
+        {
+            move.enabled = true;
+            move.Play("walk");
+            transform.position += transform.forward * Time.deltaTime * speed;
+        }
+        //stop de loop animatie als je niet meer beweegt
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            move.enabled = false;
+        }
+        // draai naar links
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(-Vector3.up * velocid * Time.deltaTime);
+        }
+        // draai naar rechts
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up * velocid * Time.deltaTime);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            move.enabled = true;
+            move.Play("jump");
+            move.Play("jump", -1, 0f);
+        }
+
+        if (Input.GetKey(KeyCode.S)) 
+        {
+            transform.position -= transform.forward * Time.deltaTime * speed;
+            move.enabled = true;
+            move.Play("walk");
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            move.enabled = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            rb.velocity += jumpSpeed * Vector3.up;
+        }
+    }
+
+    /*private void OnTriggerEnter(Collider muur)
+    {
+        SceneManager.LoadScene("stageProject");
+    }*/
+}
